c6817970a6cd32ed46d5e332c532965be928eebb
edwin6666 <edwinmendoza0620@gmail.com>
2023-06-30 16:23:59 -0400

    Feat Navbar,necessary paths and style

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import { Toaster } from "react-hot-toast";
 import Signup from './components/Signup';
 import Login from './components/Login';
 import RequiresAuth from "./components/RequiresAuth";
-
+import { HomePageUser } from "./pages/HomePageUser";
+import Navbar from "./components/Navbar/Navbar";
+import { useLocation } from "react-router-dom";
+import ContactForm from "./pages/Contact";
 function App() {
-  
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
 
   return (
-   
+    <div
+      className="bg-cover h-screen w-screen overflow-y-scroll"
+      style={{
+        backgroundImage: isLoginPage
+          ? 'none'
+          : 'url(https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80)',
+      }}
+    >
       <PostProvider>
-
+    <Navbar />
         <Routes>
           {/* Public routes */}
           <Route path="signup" exact element={ <Signup/> }/>
           <Route path ="login" exact element={ <Login/> }/>
+          <Route path="/homeuser" exact element={ <HomePageUser/>} />
+          <Route path="/contact" exact element={ <ContactForm/>} />
           
           {/* Private routes */}
           <Route element={<RequiresAuth allowedRoles={["admin"]}/>}>
@@ -25,19 +38,13 @@ function App() {
             <Route path="/new" exact element={ <PostForm/>} />
           </Route>
 
-
-          <Route element={<RequiresAuth allowedRoles={["user"]}/>}>
-           
-            
-          </Route>
-
           {/* Catch all */}
           <Route path="*" element={<NotFoundPage />} />
 
         </Routes>
         <Toaster/>
       </PostProvider>
-    
+      </div>
   );
 }
 
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -43,11 +43,22 @@ export function PostCard({ post }) {
   };
 
   return (
-    <div
-      className="bg-zinc-800 text-white rounded-sm shadow-md shadow-black hover:bg-zinc-700
-        hover:cursor-pointer"
-    >
-      <div className="px-4 py-7">
+     
+    <div className="container mx-auto max-w-sm bg-white rounded-xl shadow-lg hover:scale-105 hover:shadow-2xl transform transition-all duration-500 m-10">
+      <div className="flex items-center justify-between px-4">
+        <div className="flex justify-between items-center py-4">
+          <img className="w-12 rounded-full" src="https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="Alex" />
+          <div className="ml-3">
+            <h1 className="text-xl font-bold text-gray-800 cursor-pointer">TechTalk</h1>
+            <p className="text-sm text-gray-800 hover:underline cursor-pointer">{post.createdAt}</p>
+          </div>
+        </div>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+          </svg>
+        </div>
+      </div>
       <button
             className="bg-sky-400 text-sm px-2 py-1 rounded-sm"
             onClick={() => navigate(`/posts/${post._id}`)}
@@ -60,15 +71,13 @@ export function PostCard({ post }) {
           >
             Delete
           </button>
-        <div className="flex justify-between uppercase py-2">
-          <h3>{post.title}</h3>
-          
-        </div>
-        <p>{post.description}</p>
-        <div className=" flex justify-center items-center ">
-        {post.image && <img   src={post.image.url} className="w-full h-full object-cover " alt="imagen proporcionada por el Moderador" />}
-        </div>
+      {post.image && <img src={post.image.url}  alt="imagen " />}
+      <div className="p-6">
+        
+        <h2 className="text-xl text-gray-800 font-semibold">{post.title}</h2>
+        <p className="text-lg font font-thin  text-black">{post.description}</p>
       </div>
     </div>
-  );
-}
+  
+);
+};
