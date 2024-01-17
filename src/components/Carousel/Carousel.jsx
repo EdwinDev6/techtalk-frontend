84cc0ca0fb2c5ee324f865d7d89ed79f60aa63a6
Edwin Mendoza Rodriguez <49256757+Edwin6666@users.noreply.github.com>
2024-01-17 16:52:47 -0400

    refactor: size carousel (#45)

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,32 +1,32 @@
-'use client';
+"use client";
 
-import { Carousel } from 'flowbite-react';
+import { Carousel } from "flowbite-react";
 
-export  function DefaultCarousel() {
+export function DefaultCarousel() {
   return (
-    <Carousel>
-      <img
-        alt="..."
-        src="https://i2.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Mano-humana-y-robo%CC%81tica-tocan-un-cerebro-digital-1.jpg?w=1280&ssl=1"
-      />
-      <img
-        alt="..."
-        src="https://i1.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Steve-Jobs-presenta-el-primer-iPhone-en-2007-1.jpg?w=1280&ssl=1"
-      />
-      <img
-        alt="..."
-        src="https://i2.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Mujer-immersa-en-videojuego-con-realidad-virtual.jpg?w=1280&ssl=1"
-      />
-      <img
-        alt="..."
-        src="https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80"
-      />
-      <img
-        alt="..."
-        src="https://i2.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Coche-auto%CC%81nomo-con-HUD-Head-Up-Display.-Vehi%CC%81culo-auto%CC%81nomo-en-las-calles-de-la-ciudad.png?w=1280&ssl=1"
-      />
-    </Carousel>
-  )
+    <div className="hidden md:block">
+      <Carousel className="h-96 top-3 ">
+        <img
+          alt="img Hot Topic"
+          src="https://i2.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Mano-humana-y-robo%CC%81tica-tocan-un-cerebro-digital-1.jpg?w=1280&ssl=1"
+        />
+        <img
+          alt="img Hot Topic"
+          src="https://i1.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Steve-Jobs-presenta-el-primer-iPhone-en-2007-1.jpg?w=1280&ssl=1"
+        />
+        <img
+          alt="img Hot Topic"
+          src="https://i2.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Mujer-immersa-en-videojuego-con-realidad-virtual.jpg?w=1280&ssl=1"
+        />
+        <img
+          alt="img Hot Topic"
+          src="https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80"
+        />
+        <img
+          alt="img Hot Topic"
+          src="https://i2.wp.com/www.revistamercado.do/wp-content/uploads/2021/08/Coche-auto%CC%81nomo-con-HUD-Head-Up-Display.-Vehi%CC%81culo-auto%CC%81nomo-en-las-calles-de-la-ciudad.png?w=1280&ssl=1"
+        />
+      </Carousel>
+    </div>
+  );
 }
-
-
